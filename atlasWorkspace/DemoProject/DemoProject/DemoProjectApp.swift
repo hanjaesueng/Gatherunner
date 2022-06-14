d0bede773cae968e269b5525861f8c8ec8f87864
atlas <djlim@rsquare.co.kr>
2022-06-14 20:33:03 +0900

    [init] project

---
+++ atlasWorkspace/DemoProject/DemoProject/DemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectApp.swift
+//  DemoProject
+//
+//  Created by Atlas on 2022/06/14.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
